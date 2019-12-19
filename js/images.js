b152961772a120509aa93796b536643486981e19
Kristijan <kaledron@yahoo.com>
2019-12-19 20:12:59 +0100

    added 4,5,6 and 7th animation needs approval

--- js/images.js
+++ js/images.js
@@ -1,12 +1,46 @@
 var anime3 = document.getElementById('anime3');
+var anime4 = document.getElementById('anime4');
+var anime5 = document.getElementById('anime5');
+var anime6 = document.getElementById('anime6');
+var anime7 = document.getElementById('anime7');
 
-function getSrc(scrollPos, elem) {
-  if (scrollPos < 3200) return generatePictureCode(1, elem);
-  if (scrollPos < 3400) return generatePictureCode(2, elem);
-  if (scrollPos < 3600) return generatePictureCode(3, elem);
-  if (scrollPos < 3800) return generatePictureCode(4, elem);
-  if (scrollPos < 4000) return generatePictureCode(5, elem);
-  return generatePictureCode(5, elem);
+function getSrc3(scrollPos, elem) {
+    if (scrollPos < 3500) return generatePictureCode(1, elem);
+    if (scrollPos < 3600) return generatePictureCode(2, elem);
+    if (scrollPos < 3700) return generatePictureCode(3, elem);
+    if (scrollPos < 3800) return generatePictureCode(4, elem);
+    if (scrollPos > 3800) return generatePictureCode(5, elem);
+}
+
+function getSrc4(scrollPos, elem) {
+    if (scrollPos < 5560) return generatePictureCode(1, elem);
+    if (scrollPos < 5650) return generatePictureCode(2, elem);
+    if (scrollPos < 5740) return generatePictureCode(3, elem);
+    if (scrollPos < 5830) return generatePictureCode(4, elem);
+    if (scrollPos > 5830) return generatePictureCode(5, elem);
+}
+
+function getSrc5(scrollPos, elem) {
+    if (scrollPos < 7000) return generatePictureCode(1, elem);
+    if (scrollPos < 7250) return generatePictureCode(2, elem);
+    if (scrollPos < 7500) return generatePictureCode(3, elem);
+    if (scrollPos > 7500) return generatePictureCode(4, elem);
+}
+
+function getSrc6(scrollPos, elem) {
+    if (scrollPos < 9000) return generatePictureCode(1, elem);
+    if (scrollPos < 9100) return generatePictureCode(2, elem);
+    if (scrollPos < 9200) return generatePictureCode(3, elem);
+    if (scrollPos > 9200) return generatePictureCode(4, elem);
+}
+
+function getSrc7(scrollPos, elem) {
+    if (scrollPos < 10800) return generatePictureCode(1, elem);
+    if (scrollPos < 10860) return generatePictureCode(2, elem);
+    if (scrollPos < 10920) return generatePictureCode(3, elem);
+    if (scrollPos < 10980) return generatePictureCode(4, elem);
+    if (scrollPos < 11040) return generatePictureCode(5, elem);
+    if (scrollPos > 11040) return generatePictureCode(6, elem);
 }
 
 function generatePictureCode(val, elem) {
@@ -15,5 +49,9 @@ function generatePictureCode(val, elem) {
 
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
-    anime3.innerHTML = getSrc(window.scrollY, "anime3");
+    anime3.innerHTML = getSrc3(window.scrollY, "anime3");
+    anime4.innerHTML = getSrc4(window.scrollY, "anime4");
+    anime5.innerHTML = getSrc5(window.scrollY, "anime5");
+    anime6.innerHTML = getSrc6(window.scrollY, "anime6");
+    anime7.innerHTML = getSrc7(window.scrollY, "anime7");
 });
