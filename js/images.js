792a4eef1644d7a2be0d971070495622cce55ac3
Kristijan <kaledron@yahoo.com>
2019-12-19 08:59:33 +0100

    third animation finished

--- js/images.js
+++ js/images.js
@@ -1,17 +1,19 @@
-var zgrada2 = document.getElementById('zgrada2');
+var anime3 = document.getElementById('anime3');
 
 function getSrc(scrollPos, elem) {
-  if (scrollPos < 2000) return generatePictureCode(1, elem);
-  if (scrollPos < 2040) return generatePictureCode(2, elem);
-  if (scrollPos < 2080) return generatePictureCode(3, elem);
-  return generatePictureCode(1, elem);
+  if (scrollPos < 3200) return generatePictureCode(1, elem);
+  if (scrollPos < 3400) return generatePictureCode(2, elem);
+  if (scrollPos < 3600) return generatePictureCode(3, elem);
+  if (scrollPos < 3800) return generatePictureCode(4, elem);
+  if (scrollPos < 4000) return generatePictureCode(5, elem);
+  return generatePictureCode(5, elem);
 }
 
 function generatePictureCode(val, elem) {
-    return '<picture class="'+elem+'-rise"><source media="(min-width: 1450px)" srcset="img/'+elem+'-'+val+'.png"><source media="(min-width: 1024px)" srcset="img/'+elem+'-'+val+'.png"><img src="img/'+elem+'-'+val+'.png"></picture>';
+    return '<picture><source media="(min-width: 1450px)" srcset="img/'+elem+'-'+val+'.png"><source media="(min-width: 1024px)" srcset="img/'+elem+'-'+val+'.png"><img src="img/'+elem+'-'+val+'.png"></picture>';
 }
 
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
-    zgrada2.innerHTML = getSrc(window.scrollY, "zgrada2");
+    anime3.innerHTML = getSrc(window.scrollY, "anime3");
 });
